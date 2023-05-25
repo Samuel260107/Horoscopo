2751f2b716b4b494a2a975efb45418c6cfd45f34
Samuel260107 <130668952+Samuel260107@users.noreply.github.com>
2023-05-25 08:55:55 -0300

    commit do samuel

--- index.php
+++ index.php
@@ -8,9 +8,23 @@
     <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
     <script src='main.js'></script>
 </head>
+<style>
+form{
+    position: absolute;
+    background-color: black;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    color: white;
+    text-align: center;
+    padding: 20px 20px 40px 20px;
+    border-radius: 10px;
+}
+</style>
 <body>
     <form action="index1.php" method="get">
-        <br><label for="lblData">Escolha uma data: </label>
+        <h1>HORÓSCOPO</h1>
+        <br><label for="lblData">Escolha uma data: </label><br>
         <input type="date" name="data">
         <input type="submit">
     </form>
